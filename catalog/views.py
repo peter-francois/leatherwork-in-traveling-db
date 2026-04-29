a0d6465f9c052f8e96d579caacb36cc2d413192d
peter-francois <contact@peterfrancois.dev>
2026-04-29 11:25:24 +0200

    feat: add catalog views

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,37 @@
 from django.shortcuts import render
+from .models import Product
+from .choices import Category
+from .utils import use_filter, pagination
 
-# Create your views here.
+def product_list(request, category=None):
+    products = Product.objects.filter(available=True)
+    
+    if category:
+        products = products.filter(category=category)
+
+    products = sorted(products, key=lambda p: (p.pending_in_cart, -p.id))
+    products, form, number_of_products_in_filter, filter_used = use_filter(
+        request, products, is_all_products=category is None
+    )
+    page_obj = pagination(request, products)
+
+    return render(request, 'catalog/product_list.html', {
+        'products': page_obj,
+        'form': form,
+        'category': category,
+        'number_of_products_in_filter': number_of_products_in_filter,
+        'filter_used': filter_used,
+    })
+
+
+def produits(request):
+    return product_list(request)
+
+def maroquinerie(request):
+    return product_list(request, category=Category.MAROQUINERIE)
+
+def macrames(request):
+    return product_list(request, category=Category.MACRAME)
+
+def hybride(request):
+    return product_list(request, category=Category.HYBRIDE)
