587df45bf5d5abdf3a5c0868b3d30068716ccb79
peter-francois <contact@peterfrancois.dev>
2026-04-30 12:26:37 +0200

    refactor(cart): creat Cart and CartItem model and migrate data from page_vente models

---
+++ cart/migrations/0002_migrate_products.py
@@ -0,0 +1,43 @@
+from django.db import migrations
+
+def migrate_carts(apps, schema_editor):
+    OldCart  = apps.get_model('page_vente', 'Cart')
+    NewCart  = apps.get_model('cart', 'Cart')
+
+    for obj in OldCart.objects.all():
+        NewCart.objects.create(
+            id=obj.id,
+            session_id=obj.session_id,
+            uuid=obj.uuid,
+            cgv_accepted=obj.cgv_accepted,
+            cgv_accepted_at=obj.cgv_accepted_at,
+            cgv_expires_at=obj.cgv_expires_at,
+            created_at=obj.created_at,
+            cart_expires_at=obj.cart_expires_at,
+            paid=obj.paid,
+            paid_at=obj.paid_at,
+        )
+
+def migrate_cart_items(apps, shema_editor):
+    OldCartItem = apps.get_model('page_vente', 'CartItem')
+    NewCartItem = apps.get_model('cart', 'CartItem')
+
+    for obj in OldCartItem.objects.all():
+        NewCartItem.objects.create(
+            id = obj.id,
+            cart_id = obj.cart_id,
+            product_id = obj.product_id,
+            quantity = obj.quantity
+        )
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('cart', '0001_initial'),
+        ('page_vente', '0048_alter_cart_cgv_accepted'),
+    ]
+
+    operations = [
+        migrations.RunPython(migrate_carts),
+        migrations.RunPython(migrate_cart_items), 
+    ]
--- leatherwork/settings.py
+++ leatherwork/settings.py
@@ -61,7 +61,8 @@ LOCAL_APPS = [
     'page_vente',
     'core',
     'legal',
-    'catalog'
+    'catalog',
+    'cart'
 ]
 
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
