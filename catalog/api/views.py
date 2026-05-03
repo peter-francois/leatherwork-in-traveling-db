5d60b7e2e0180db7c0de1935c4676b8ad22a0b15
Peter Francois <contact@peterfrancois.dev>
2026-05-03 16:33:41 +0200

    refactor(catalog): move api get_product_images from page_vente to catalog

---
+++ catalog/api/views.py
@@ -0,0 +1,20 @@
+from django.http import JsonResponse
+
+from catalog.models import Product
+
+
+def get_product_images(request, article_id):
+    try:
+        product = Product.objects.get(id=article_id)
+        images = [
+            product.image1.url if product.image1 else None,
+            product.image2.url if product.image2 else None,
+            product.image3.url if product.image3 else None,
+            product.image4.url if product.image4 else None,
+            product.image5.url if product.image5 else None,
+            product.image6.url if product.image6 else None,
+            ]
+        images = [image for image in images if image]
+        return JsonResponse({'images': images, 'nom': product.name, 'description': product.description if product.description else None, 'discount': product.discount,'prix': product.price, 'en_attente_dans_panier': product.pending_in_cart, 'sur_commande': product.on_demand})
+    except Product.DoesNotExist:
+        return JsonResponse({'error': 'Product not found'}, status=404)
