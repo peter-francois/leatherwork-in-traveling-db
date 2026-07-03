ab031ef3491abd9f1b1065fbea4a9528a800c697
peter-francois <contact@peterfrancois.dev>
2026-07-03 11:19:08 +0200

    chore(seo): set product detail pages to noindex until content is ready
    - Add noindex meta to product_detail.html while product names and
      descriptions are being optimized for SEO
    - Remove product detail URLs from sitemap until pages are ready to index

--- core/views.py
+++ core/views.py
@@ -5,7 +5,6 @@ from django.views.decorators.csrf import ensure_csrf_cookie
 
 from core.sitemaps import StaticSitemap
 from core.sitemaps_category import CategorySitemap
-from core.sitemaps_product_detail import ProductDetailSitemap
 
 from .services import generate_sitemap_index
 
@@ -31,7 +30,7 @@ def sitemap_lang(request, lang):
     sitemaps = {
         "static": StaticSitemap(lang),
         "categories": CategorySitemap(lang),
-        "products": ProductDetailSitemap(lang),
+        # "products": ProductDetailSitemap(lang),
     }
     return django_sitemap(request, sitemaps)
 
